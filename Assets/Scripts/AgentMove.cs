b761761f7fabe6af71f9b69e9c74494a44105107
harrisongiger <harrisongiger21@gmail.com>
2021-12-07 13:39:41 -0800

    for

--- Assets/Scripts/AgentMove.cs
+++ Assets/Scripts/AgentMove.cs
@@ -10,15 +10,21 @@ public class AgentMove : MonoBehaviour
     public Transform home;
     public Transform home2;
     public Transform home3;
+    public Transform home4;
     NavMeshAgent _navMeshAgent;
     public GameObject agent;
+    public Material US;
+    public Material France;
+    public Material Germany;
+    public Material Japan;
+    public Material Other;
 
     void Start()
     {
 
         agent = gameObject;
 
-        type = Mathf.Floor(Random.Range(0, 3));
+        type = Mathf.Floor(Random.Range(0, 5));
 
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
 
@@ -35,10 +41,18 @@ public class AgentMove : MonoBehaviour
                 _navMeshAgent.SetDestination(home2.position);
             } else if (type == 2) {
                 _navMeshAgent.SetDestination(home3.position);
+            } else if (type == 3) {
+                _navMeshAgent.SetDestination(home4.position);
+            } else if (type == 4) {
+                _navMeshAgent.SetDestination(home4.position);
             }
 
             
         }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
 }
